38d90ce165233fe30b6d7d08ce2c2516ba3a27f2
Jonathan Adams <jd.adams16@gmail.com>
2016-03-01 22:04:47 -0600

    Added channel and handler definition for installing packages from a Git repo.

---
+++ R/.gitPackageChannel.R
@@ -0,0 +1,33 @@
+#' A function that defines a channel and handles messages for installing Git packages.
+#'
+#' \code{.gitPackageChannel} is a function that should be passed to \code{minionListener}
+#' whenever you want to add a listener and handler for installing packages from a Git
+#' repository. It tells the listener what channel to listen on and defines a callback
+#' for handling any messages on this channel.
+#'
+#' The handler for \code{.gitPackageChannel} expects package installation messages to be
+#' lists with one required key and two optional keys. The required key is \code{url},
+#' and the optional keys are \code{subdir} and \code{branch}. For more information on
+#' these values, see the documentation for \code{install_git} from the \code{devtools}
+#' package.
+#'
+#' @export
+#'
+#' @param channel The channel to listen for git package installation messages. Defulats
+#'   to \code{gitPackage}.
+
+.gitPackageChannel <- function(channel = "gitPackage") {
+    callback <- function(message) {
+        install_git(
+            url = message$url,
+            subdir = message$subdir,
+            branch = message$branch
+        )
+    }
+    return(
+        list(
+            channel = channel,
+            callback = callback
+        )
+    )
+}
