5ceb7630ba6d4ccd32651bee787a1da9b30b20b1
Jonathan Adams <jd.adams16@gmail.com>
2016-06-19 01:16:21 -0500

    Draw distributional parameters from other distributions.

--- stealTheMoonSim.R
+++ stealTheMoonSim.R
@@ -1,53 +1,12 @@
 stealTheMoonSim <- function(params) {
-    # Assume base fuel required (in millions of pounds) is lognormally distributed with meanlog 0.25 and sdlog 0.25
-    fuelParams <- list(
-        meanlog = 0.25,
-        sdlog = 0.25
-    )
+    surfaceFuelDependenceCop <- copula::claytonCopula(params$surfaceFuelDependenceCopParams$param, dim = 2)
 
-    # Assume traveling to moon at peak solar flare season and that the number of flares for the trip can be
-    # modeled as Poisson with mean 4.
-    solarFlareParams <- list(
-        lambda = 4
-    )
+    baseFuel <- rlnorm(1, meanlog = params$fuelParams$meanlog, sdlog = params$fuelParams$sdlog)
 
-    # Assume flare emitting surface (as a multiple of 100) for each flare is gamma distributed with shape 6 and
-    # scale 0.75
-    flareSurfaceParams <- list(
-        shape = 6,
-        scale = 0.75
-    )
-
-    # Assume additional amount of fuel required due to flares (in 100,000 pounds) is lognormally distributed with
-    # meanlog 0.2 and sdlog 0.5
-    flareAdditionalFuelParams <- list(
-        meanlog = 0.2,
-        sdlog = 0.5
-    )
-
-    # Assume dependence structure between flare emitting surface and additional Fuel required can be modeled
-    # with a Clayton copula with parameter 10
-    surfaceFuelDependenceCop <- copula::claytonCopula(10, dim = 2)
-
-    # TODO: Try to add asteroid model
-    # An asteroid shower is expected to occur while travelling too and from the Moon. Assume the travel path
-    # to moon has been divided into 10,000,000 segments and that the number of asteroids we have to dodge in any
-    # segment is distributed Poisson with lambda 0.000005.
-    asteroidParams <- list(
-        lambda = 0.000005
-    )
-
-    # Assume the additional amount of fuel required (in 100 pounds) for dodging an asteroid is normally
-    # distributed with mean 1.5 and sd 0.25.
-    asteroidAdditionalFuelParams <- list(
-        mean = 1.5,
-        sd = 0.25
-    )
-
-    baseFuel <- rlnorm(1, meanlog = fuelParams$meanlog, sdlog = fuelParams$sdlog)
-    numSolarFlares <- rpois(1, lambda = solarFlareParams$lambda)
-    flareSurface <- rgamma(numSolarFlares, shape = flareSurfaceParams$shape, scale = flareSurfaceParams$scale)
-    flareSurfaceUnif <- pgamma(flareSurface, shape = flareSurfaceParams$shape, scale = flareSurfaceParams$scale)
+    # Solar flares occurring during trip
+    numSolarFlares <- rpois(1, lambda = params$solarFlareParams$lambda)
+    flareSurface <- rgamma(numSolarFlares, shape = params$flareSurfaceParams$shape, scale = params$flareSurfaceParams$scale)
+    flareSurfaceUnif <- pgamma(flareSurface, shape = params$flareSurfaceParams$shape, scale = params$flareSurfaceParams$scale)
     flareAdditionalFuelUnif <- copula::rtrafo(
         u = as.matrix(
             cbind(
@@ -60,14 +19,14 @@ stealTheMoonSim <- function(params) {
         inverse = T
     )
     # Additional required fuel due to solar flares, converted to millions of pounds
-    flareAdditionalFuel <- qlnorm(flareAdditionalFuelUnif, meanlog = flareAdditionalFuelParams$meanlog, sdlog = flareAdditionalFuelParams$sdlog)/10
+    flareAdditionalFuel <- qlnorm(flareAdditionalFuelUnif, meanlog = params$flareAdditionalFuelParams$meanlog, sdlog = params$flareAdditionalFuelParams$sdlog)/10
 
     # Asteroids along path
-    asteroidsOnPath <- rpois(10000000, lambda = asteroidParams$lambda)
+    asteroidsOnPath <- rpois(10000000, lambda = params$asteroidParams$lambda)
     asteroidSegments <- which(asteroidsOnPath > 0)
     numAsteroids <- sum(asteroidsOnPath)
     # Additional required fuel due to dodging asteroids, converted to millions of pounds
-    asteroidAdditionalFuel <- rnorm(numAsteroids, mean = asteroidAdditionalFuelParams$mean, sd = asteroidAdditionalFuelParams$sd) / 10000
+    asteroidAdditionalFuel <- rnorm(numAsteroids, mean = params$asteroidAdditionalFuelParams$mean, sd = params$asteroidAdditionalFuelParams$sd) / 10000
 
     totalFuel <- baseFuel + sum(flareAdditionalFuel) + sum(asteroidAdditionalFuel)
 
