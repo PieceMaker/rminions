37f6e64b6ecf4f318c29269204934737b3478fbf
Jonathan Adams <jd.adams16@gmail.com>
2018-03-18 19:48:31 -0500

    Start converting worker to execute functions from packages.

--- R/minionWorker.R
+++ R/minionWorker.R
@@ -4,8 +4,8 @@
 #' running it. It immediately performs some housekeeping by gathering logging information
 #' and connecting to the message queue. Once connected, it performs a blocking pop on the
 #' \code{jobsqueue} and waits until it receives a bundled job message. Once a message is
-#' received, it runs the bundled function with the bundled parameters and stores any
-#' returned results in the bundled response key.
+#' received, it pulls the specified function for the specified package and runs it with
+#' the bundled parameters and stores any returned results in the bundled response key.
 #'
 #' \code{minionWorker} is the core of the \code{redis-minions} package and multiple R
 #' processes running this function should be spawned. The goal is to spawn just enough
@@ -15,25 +15,25 @@
 #'
 #' The minion worker is constructed to work with nearly all tasks. In order to accomplish
 #' this, job messages need to be of a specific format. Job message must be lists with three
-#' keys: \code{Function}, \code{Parameters}, \code{ResultsQueue}, \code{ErrorQueue}, and
-#' optionally \code{Packages}.
+#' keys: \code{package}, \code{func}, \code{parameters}, \code{resultsQueue}, and
+#' \code{errorQueue}.
 #'
-#' \code{Function} is the main function that controls the job and holds the core logic. Even
-#' if the desired job is a simple script, the script should be wrapped in a function which
-#' can then be passed to the worker. For more complex jobs that call subfunctions, it is
-#' recommended that you create a package with these subfunctions and have \code{Function}
-#' load this package. If \code{Function} must take input paremeters, define it as as a
-#' function of one parameter (here referenced as \code{Parameters}), i.e.
-#' \code{func(Parameters)}. Parameters will be a list and every desired parameter will be
-#' a key.
+#' \code{package} is the name of the package containing the function that will be executed.
 #'
-#' \code{Parameters} is a list of all the parameters to be passed to \code{Function}.
+#' \code{func} is the name of the function, contained in the specified package, that you
+#' wish to execute with this request. If \code{useJSON} is false, \code{func} can take any
+#' type of parameter. If \code{useJSON} is true, then \code{func} should only take R data
+#' as parameters that can be converted to and from JSON, i.e. strings, numbers, simple vectors,
+#' etc.
 #'
-#' \code{ResultsQueue} will be a string with the name of the redis queue to store any
+#' \code{parameters} is a named list of all the parameters to be passed to \code{func}. The
+#' names must match those of the parameters \code{func} expects.
+#'
+#' \code{resultsQueue} will be a string with the name of the redis queue to store any
 #' returned results in.
 #'
-#' \code{ErrorQueue} will be a string with the name of the redis queue to store any errors
-#' thrown while running the job.
+#' \code{errorQueue} will be a string with the name of the redis queue to store any errors
+#' thrown while running the job. Note that returned errors
 #'
 #' \code{Packages} is an optional key. This accepts a vector of string(s) containing the
 #' name(s) of any package(s) that need to be loaded before running \code{Function}. This
@@ -41,7 +41,7 @@
 #' and you need to load it beforehand. The package(s) will be unloaded after \code{Function}
 #' has finished running to help prevent memory issues.
 #'
-#' @import plyr rredis R.utils Rbunyan
+#' @import plyr rredis R.utils Rbunyan jsonlite
 #'
 #' @export
 #'
@@ -55,8 +55,12 @@
 #' @param logFileDir A string giving the directory to store worker log files if logging is
 #'   enabled. Defaults to \code{/var/log/R}. Set to \code{stdout} to output logs to
 #'   standard out.
+#' @param useJSON Flag specifying whether jobs and results will be sent in JSON format. Defaults to false
+#    so R-specific objects are preserved in transit. If sending jobs from languages other than R, set useJSON
+#    to true and make sure jobs are defined in the JSON format and the function being executed does not require
+#    any R-specific objects.
 
-minionWorker <- function(host, port = 6379, jobsQueue = "jobsqueue", logLevel = 'DEBUG', logFileDir = "/var/log/R") {
+minionWorker <- function(host, port = 6379, jobsQueue = "jobsqueue", logLevel = 'DEBUG', logFileDir = "/var/log/R", useJSON = F) {
     workerHost <- as.character(R.utils::System$getHostname())
     workerID <- paste0(workerHost, '-worker-', Sys.getpid())
     if(logFileDir == 'stdout') {
@@ -92,42 +96,68 @@ minionWorker <- function(host, port = 6379, jobsQueue = "jobsqueue", logLevel = 
         tryCatch(
             {
                 job <- rredis::redisBRPopLPush(jobsQueue, workerID)
-
-                packages <- job$Packages
-                if(!is.null(packages)) {
-                    plyr::a_ply(packages, 1, library, character.only = T)
+                if(useJSON) {
+                    job <- jsonlite::fromJSON(job)
                 }
-                func <- job$Function
-                params <- job$Parameters
-                #Pass in redis connection in case it is needed inside func
-                params$redisConn <- conn
-                resultsQueue <- job$ResultsQueue
-                errorQueue <- job$ErrorQueue
-
+                package <- job$package
+                func <- job$func
+                params <- job$parameters
+                resultsQueue <- job$resultsQueue
+                errorQueue <- job$errorQueue
+                if(is.null(errorQueue)) {
+                    errorQueue <- resultsQueue
+                }
                 if(is.null(errorQueue)) {
                     Rbunyan::bunyanLog.error("ErrorQueue not provided.")
-                    job$Error <- "ErrorQueue not provided."
+                    job$error <- "ErrorQueue not provided."
+                    job$status <- "failed"
+                    if(useJSON) {
+                        job <- jsonlite::toJSON(job)
+                    }
                     rredis::redisRPush(
                         "missingErrorQueueErrors",
                         job
                     )
+                } else if(is.null(package)) {
+                    Rbunyan::bunyanLog.error("Package not provided.")
+                    job$error <- "Package not provided."
+                    job$status <- "failed"
+                    if(useJSON) {
+                        job <- jsonlite::toJSON(job)
+                    }
+                    rredis::redisRPush(
+                        "errorQueue",
+                        job
+                    )
                 } else if(is.null(func)) {
                     Rbunyan::bunyanLog.error("Function not provided.")
-                    job$Error <- "Function not provided."
+                    job$error <- "Function not provided."
+                    job$status <- "failed"
+                    if(useJSON) {
+                        job <- jsonlite::toJSON(job)
+                    }
                     rredis::redisRPush(
                         "errorQueue",
                         job
                     )
                 } else if(is.null(params)) {
                     Rbunyan::bunyanLog.error("Parameters not provided.")
-                    job$Error <- "Parameters not provided."
+                    job$error <- "Parameters not provided."
+                    job$status <- "failed"
+                    if(useJSON) {
+                        job <- jsonlite::toJSON(job)
+                    }
                     rredis::redisRPush(
                         "errorQueue",
                         job
                     )
                 } else if(is.null(resultsQueue)) {
                     Rbunyan::bunyanLog.error("ResultsQueue not provided.")
-                    job$Error <- "ResultsQueue not provided."
+                    job$error <- "ResultsQueue not provided."
+                    job$status <- "failed"
+                    if(useJSON) {
+                        job <- jsonlite::toJSON(job)
+                    }
                     rredis::redisRPush(
                         "errorQueue",
                         job
@@ -135,15 +165,32 @@ minionWorker <- function(host, port = 6379, jobsQueue = "jobsqueue", logLevel = 
                 } else {
                     tryCatch(
                         {
-                            results <- func(params)
-                            Rbunyan::bunyanLog.debug(
-                                sprintf(
-                                    "Sending results to queue %s: %s",
-                                    resultsQueue,
-                                    jsonlite::serializeJSON(results)
+                            func <- getFromNamespace(func, ns = package)
+                            # #Pass in redis connection in case it is needed inside func
+                            # params$redisConn <- conn # TODO: This now breaks with do.call. Decide whether to keep or remove.
+                            # TODO: Try to make the below call safer
+                            results <- do.call(func, params)
+                            job$results <- results
+                            job$status <- "succeeded"
+                            if(useJSON) {
+                                job <- jsonlite::toJSON(job)
+                                Rbunyan::bunyanLog.debug(
+                                    sprintf(
+                                        "Sending results to queue %s: %s",
+                                        resultsQueue,
+                                        job
+                                    )
                                 )
-                            )
-                            rredis::redisRPush(resultsQueue, results)
+                            } else {
+                                Rbunyan::bunyanLog.debug(
+                                    sprintf(
+                                        "Sending results to queue %s: %s",
+                                        resultsQueue,
+                                        jsonlite::serializeJSON(job)
+                                    )
+                                )
+                            }
+                            rredis::redisRPush(resultsQueue, job)
                         },
                         error = function(e) {
                             Rbunyan::bunyanLog.error(
@@ -152,21 +199,12 @@ minionWorker <- function(host, port = 6379, jobsQueue = "jobsqueue", logLevel = 
                                     e
                                 )
                             )
-                            job$Error <- e
+                            job$error <- e
+                            job$status <- "failed"
                             rredis::redisRPush(errorQueue, job)
                         },
                         finally = {
                             rredis::redisDelete(workerID)
-                            if(!is.null(packages)) {
-                                plyr::a_ply(
-                                    packages,
-                                    1,
-                                    function(package) {
-                                        package <- paste0("package:", package)
-                                        detach(package, unload = T, character.only = T)
-                                    }
-                                )
-                            }
                         }
                     )
                 }
@@ -179,7 +217,11 @@ minionWorker <- function(host, port = 6379, jobsQueue = "jobsqueue", logLevel = 
                     )
                 )
                 if(is.list(job)) {
-                    job$Error <- e
+                    job$error <- e
+                    job$status <- "catastrophic"
+                    if(useJSON) {
+                        job <- jsonlite::toJSON(job)
+                    }
                     if(is.null(job$errorQueue)) {
                         rredis::redisRPush('unhandledErrors', job)
                     } else {
