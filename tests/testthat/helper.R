ce19f57c6f91bb1568d3b4a8e78bd3fa49ab0a92
Jonathan Adams <jd.adams16@gmail.com>
2019-07-29 18:02:42 -0500

    Found another place where I could remove rredis.

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -9,12 +9,6 @@ testReduxConnection <- function() {
   return(redux::hiredis())
 }
 
-testRRedisConnection <- function() {
-  skipIfNoRedis()
-  conn <- rredis::redisConnect(returnRef = T)
-  return(conn)
-}
-
 cleanQueue <- function(reduxConn, queue) {
   reduxConn$DEL(queue)
   expect_equal(reduxConn$LLEN('testThatQueue'), 0)
